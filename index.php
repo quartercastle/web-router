cac5aae554fcf563b37ec1b478c54154516e3d2d
Frederik Kvartborg <fka@skemax.dk>
2016-01-12 11:08:35 +0100

    Added editorconfig, npmignore, .htaccess, index.php and package.json

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <!-- should be remove for production -->
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta charset="UTF-8">
+  <title>Skemax</title>
+  <link rel="stylesheet" href="/dist/css/app.css?v=<?= rand(0,999999) ?>">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+<body>
+  <div id="app">
+
+  </div>
+  
+  <script src="/dist/js/app.js?v=<?= rand(0,999999) ?>" async></script>
+</body>
+</html>
